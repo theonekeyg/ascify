d230c1527446e13d52fd0c85fc7cba7beefa3c6a
theonekeyg <theonekeyg@gmail.com>
2020-07-27 00:11:27 +0300

    add multithreading support

--- src/ascify.py
+++ src/ascify.py
@@ -1,7 +1,6 @@
 import sys
 import os
-import numpy as np
-from PIL import Image
+import argparse
 
 from grid import AsciiGrid
 from renditions import *
@@ -17,12 +16,23 @@ ascii_tokens = {
     ".": 0
 }
 
-
-
 def main():
-    grid = AsciiGrid("./leader.jpg")
-    grid.ascify()
-    print(grid.ascii_grid)
+    parser = argparse.ArgumentParser(description="utility to ascify image files")
+    parser.add_argument("-i", "--input", required=True, help="Input image file", dest="input")
+    parser.add_argument("-o", "--output", help="If this option is provided, the output file will be \
+                        used to store the result, stdout otherwise", dest="output")
+    parser.add_argument("-c", action="store_const", const=True, default=False,
+                        help="Whether do calculations in multiple threads or not", dest="concurrent")
+    args = parser.parse_args()
+
+    concurrent = args.concurrent
+    grid = AsciiGrid(args.input, concurrent=concurrent)
+    grid.start()
+    if args.output is None:
+        print(grid.ascii_grid)
+    else:
+        with open(args.output, "w") as f:
+            f.write(grid.ascii_grad)
 
 if __name__ == "__main__":
     main()
--- src/grid.py
+++ src/grid.py
@@ -1,12 +1,13 @@
 import sys
 import os
+import threading
 import numpy as np
 from PIL import Image
 
 from renditions import default_rendition
 
 class AsciiGrid:
-    def __init__(self, impath, step=5, size=(350, 600), rendition=None):
+    def __init__(self, impath, step=5, size=(350, 600), rendition=None, concurrent=False):
         if len(size) != 2:
             raise ValueError("provided size got invalid format, expected length "
                              f"= 2, but received: {len(size)}")
@@ -17,20 +18,44 @@ class AsciiGrid:
             self.rendition = default_rendition
         else: self.rendition = rendition
 
-        self.step       = step
-        self.y          = size[0] // step
-        self.x          = size[1] // step
-        self.img        = Image.open(impath)
-        self.img        = self.img.resize(size[::-1], resample=Image.BILINEAR)
-        self.img        = np.array(self.img)
-        self.ascii_grid = ""
+        if concurrent:
+            self.n_threads = os.cpu_count() if os.cpu_count() is not None else 4
+            self.threads      = []
+            self.shared_grids = ["" for _ in range(self.n_threads)]
+            for i in range(self.n_threads):
+                th = threading.Thread(target=self.th_ascify, args=(i,))
+                self.threads.append(th)
+        else: self.n_threads = 1
+
+        self.concurrent   = concurrent
+        self.step         = step
+        self.y            = size[0] // step // self.n_threads
+        self.x            = size[1] // step
+        self.img          = Image.open(impath).resize(size[::-1], resample=Image.BILINEAR)
+        self.img          = np.array(self.img)
+        self.ascii_grid   = ""
+
+    def start(self):
+        if self.concurrent:
+            for th in self.threads:
+                th.start()
+                th.join()
+            self.ascii_grid = self.ascii_grid.join(self.shared_grids)
+        else:
+            self.ascify();
 
     def ascify(self):
         for i in range(self.y):
             for j in range(self.x):
                 img_slice = self.img[i*self.step:(i+1)*self.step,
                                      j*self.step:(j+1)*self.step]
-                sys.stdout.write(self.rendition(img_slice))
+                self.ascii_grid += self.rendition(img_slice)
+            self.ascii_grid += "\n"
 
-            # self.ascii_grid += "\n"
-            sys.stdout.write("\n")
+    def th_ascify(self, tid):
+        for i in range(self.y*tid, self.y*(tid+1)):
+            for j in range(self.x):
+                img_slice = self.img[i*self.step:(i+1)*self.step,
+                                     j*self.step:(j+1)*self.step]
+                self.shared_grids[tid] += self.rendition(img_slice)
+            self.shared_grids[tid] += "\n"
