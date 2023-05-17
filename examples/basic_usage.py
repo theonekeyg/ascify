b23f012cea90cd431c8a7671ceec504c465cea7f
theonekeyg <theonekeyg@gmail.com>
2023-05-17 20:25:05 +0300

    Change example image

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from ascify.grid import AsciiGrid
+from ascify.renditions import truecolor_default
 
 def main():
-    grid = AsciiGrid("leader.jpg", step=5, size=(70, 120))
+    grid = AsciiGrid("crab.png", step=5, size=(70, 120), rendition=truecolor_default)
     grid.start()
     print(grid)
 
